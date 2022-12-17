16e3a957693deef04e6208dcab98dd29726b1301
SEAF39 <seaf.roshi.37@gmail.com>
2022-12-17 02:58:07 +0000

    sayHello function

---
+++ hello.js
@@ -0,0 +1,27 @@
+/*
+
+// declared a function named sayHello 
+
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+
+*/
+
+/*
+
+
+// call sayHello() three times 
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+
+*/
