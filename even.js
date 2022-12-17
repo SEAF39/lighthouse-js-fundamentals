9bd38fe0cf0ee7324f1b9f82b36dada966834ed4
SEAF39 <seaf.roshi.37@gmail.com>
2022-12-17 03:10:21 +0000

    even function

--- even.js
+++ even.js
@@ -16,6 +16,9 @@ console.log(elevenIsEven);
 */
 
 
+/*
+
+We can simplify our above code a little by learning that we don't need to set our function return values to new variables before doing something with them–we can use them directly where we need them.
 
 const isEven  = function (num) {
   return num % 2 === 0;
@@ -23,3 +26,5 @@ const isEven  = function (num) {
 
 console.log(isEven(10));
 console.log(isEven(11));
+
+*/
