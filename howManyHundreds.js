98cf4f7269a2ce2ab5ac6ea6f35461911312d005
SEAF39 <seaf.roshi.37@gmail.com>
2022-12-19 04:01:38 +0000

    howManyHundreds

---
+++ howManyHundreds.js
@@ -0,0 +1,10 @@
+function howManyHundreds(numbers){
+  const n = numbers / 100;
+  return Math.trunc(n)
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
