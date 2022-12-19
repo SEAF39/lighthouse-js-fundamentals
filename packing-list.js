b5c179282462b87243be0dcc775cb64d0587c451
SEAF39 <seaf.roshi.37@gmail.com>
2022-12-19 05:01:04 +0000

    packing-list-with while

--- packing-list.js
+++ packing-list.js
@@ -1,4 +1,6 @@
 // iteraring over array with for
+
+/*
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
@@ -6,3 +8,16 @@ for (let X = 0; X < packingList.length; X++) {
   console.log(packingList[X]);
 }
 packingList[3];
+
+*/
+
+//Iterating over array with while
+
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+let X = 0;
+while (X < packingList.length) {
+  console.log(packingList[X]);
+  X++;
+}
